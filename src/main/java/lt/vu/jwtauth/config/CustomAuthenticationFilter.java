0b532d08ad7681962b42ec9d7d855fd114fdd444
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-12-01 19:17:12 +0200

    Implement basic username password user authentication (no JWT).

--- src/main/java/lt/vu/jwtauth/config/CustomAuthenticationFilter.java
+++ src/main/java/lt/vu/jwtauth/config/CustomAuthenticationFilter.java
@@ -1,74 +1,88 @@
 package lt.vu.jwtauth.config;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.FilterChain;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import javax.annotation.Resource;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+@Component
+@ComponentScan(basePackages = "lt.vu.jwtauth.service")
+public class CustomAuthenticationFilter implements AuthenticationProvider {
 
-@Slf4j
-public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    private final AuthenticationManager authenticationManager;
+    @Resource
+    UserDetailsService userDetailsService;
 
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        String username = authentication.getName();
 
-    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+        if (StringUtils.isEmpty(username)) {
+            throw new BadCredentialsException(("Invalid login details"));
+        }
+
+        UserDetails user;
+        try {
+            user = userDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException exception) {
+            throw new BadCredentialsException("Invalid login details");
+        }
+
+        String password = authentication.getCredentials().toString();
+
+        UsernamePasswordAuthenticationToken token =  new UsernamePasswordAuthenticationToken(username, password, user.getAuthorities());
+        token.setDetails(authentication.getDetails());
+        return token;
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        log.info("Username is {}", username);
-        log.info("Password is {}", password);
-
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-
-        return authenticationManager.authenticate(authenticationToken);
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
-        User user = (User) authResult.getPrincipal();
-        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-        String access_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                .withIssuer(request.getRequestURI())
-                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                .sign(algorithm);
-
-        String refresh_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 10 * 1000))
-                .withIssuer(request.getRequestURI())
-                .sign(algorithm);
-
-        Map<String, String> tokens = new HashMap<>();
-        tokens.put("access_token", access_token);
-        tokens.put("refresh_token", refresh_token);
-
-        response.setContentType(APPLICATION_JSON_VALUE);
-
-        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-    }
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        log.info("Username is {}", username);
+//        log.info("Password is {}", password);
+//
+//        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+//
+//        return authenticationManager.authenticate(authenticationToken);
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+//        User user = (User) authResult.getPrincipal();
+//        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+//        String access_token = JWT.create()
+//                .withSubject(user.getUsername())
+//                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+//                .withIssuer(request.getRequestURI())
+//                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+//                .sign(algorithm);
+//
+//        String refresh_token = JWT.create()
+//                .withSubject(user.getUsername())
+//                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 10 * 1000))
+//                .withIssuer(request.getRequestURI())
+//                .sign(algorithm);
+//
+//        Map<String, String> tokens = new HashMap<>();
+//        tokens.put("access_token", access_token);
+//        tokens.put("refresh_token", refresh_token);
+//
+//        response.setContentType(APPLICATION_JSON_VALUE);
+//
+//        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+//    }
 }
--- src/main/java/lt/vu/jwtauth/config/SecurityConfig.java
+++ src/main/java/lt/vu/jwtauth/config/SecurityConfig.java
@@ -1,22 +1,14 @@
 package lt.vu.jwtauth.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -38,39 +30,57 @@ public class SecurityConfig {
 //        http.addFilter(customAuthenticationFilter);
 //    }
 
+    @Autowired
+    private CustomAuthenticationFilter authFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(new AuthenticationConfiguration()));
-        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-        http
-                .csrf(csrf -> csrf.disable())
-//                .formLogin(form -> form
-//                        .loginPage("/login")
-//                        .permitAll()
-//                )
-//                .authorizeHttpRequests((authz) -> authz
-//                        .anyRequest().authenticated()
-//                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//                .httpBasic(withDefaults());
-        http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+//        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(new AuthenticationConfiguration()));
+//        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+//        http
+//                .csrf(csrf -> csrf.disable())
+////                .formLogin(form -> form
+////                        .loginPage("/login")
+////                        .permitAll()
+////                )
+////                .authorizeHttpRequests((authz) -> authz
+////                        .anyRequest().authenticated()
+////                )
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+////                .httpBasic(withDefaults());
+//        http.addFilter(customAuthenticationFilter);
+//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
 
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager user() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("Vege")
-                        .password("password")
-                        .authorities("read")
-                        .build()
-        );
-    }
+//    @Bean
+//    public InMemoryUserDetailsManager user() {
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("Vege")
+//                        .password("password")
+//                        .authorities("read")
+//                        .build()
+//        );
+//    }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authFilter);
+
+        return authenticationManagerBuilder.build();
     }
 }
